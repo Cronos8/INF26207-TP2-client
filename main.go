d77c660e24a5988fe46c1e9b8c3b4524f532ff1a
Cronos8 <alexng.tp@gmail.com>
2021-03-25 17:36:42 -0400

    send and receive message

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
-func main(){
-     fmt.Println("Hello World !")
-     return
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+	name := os.Args[1]
+	conn, err := net.Dial("udp4", name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("-------------------------")
+	fmt.Printf("Connected on : %s\n\n", name)
+
+	s := "Hello Server"
+	conn.Write([]byte(s))
+	conn.Write([]byte("\r\n\r\n"))
+	log.Printf("Send: %s", s)
+
+	buff := make([]byte, 1024)
+	n, _ := conn.Read(buff)
+	log.Printf("Receive: %s\n", buff[:n])
+
+	fmt.Println("-------------------------")
 }
