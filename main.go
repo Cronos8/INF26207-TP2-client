a32ece5fa463bbff31103d97c38557396b13a4ad
Cronos8 <alexng.tp@gmail.com>
2021-04-05 00:10:59 -0400

    ajout fonction serverconnexion

--- main.go
+++ main.go
@@ -50,33 +50,9 @@ func sendPaquetWithFiability(fiability float32) bool {
 	return false
 }
 
-//func decapPacket()
+func newServerConnexion(conn net.Conn) int {
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
-		os.Exit(1)
-	}
-	name := os.Args[1]
-	conn, err := net.Dial("udp4", name)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("Local addr : %v || Local network : %v\n", conn.LocalAddr().String(), conn.LocalAddr().Network())
-	fmt.Printf("Remote addr : %v || Remote network : %v\n", conn.RemoteAddr().String(), conn.RemoteAddr().Network())
-	defer conn.Close()
-
-	buff := make([]byte, 1024)
 	buffCo := make([]byte, 1000)
-	var packetMap map[uint64]int
-
-	packetMap = make(map[uint64]int)
-	str := ""
-
-	conn.Write([]byte("Client - CONNEXION OK"))
-	fmt.Println("-------------------------")
-	fmt.Printf("Connected on : %s\n\n", name)
 
 	for {
 		n, err := conn.Read(buffCo)
@@ -91,63 +67,73 @@ func main() {
 			break
 		}
 	}
+	return 0
+}
 
-	for /*z := 0; z < 10; z++*/ {
-		fmt.Println("-------------------------")
-		//fmt.Println("dan for")
-		n, err := conn.Read(buff)
-		//fmt.Println(n)
-		//fmt.Println(err)
-		//fmt.Println("dan hey")
-		if err != nil {
-			fmt.Println("ERROR")
-			break
-		}
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+	name := os.Args[1]
+	conn, err := net.Dial("udp4", name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error %s\n", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Local addr : %v || Local network : %v\n", conn.LocalAddr().String(), conn.LocalAddr().Network())
+	fmt.Printf("Remote addr : %v || Remote network : %v\n", conn.RemoteAddr().String(), conn.RemoteAddr().Network())
+	defer conn.Close()
 
-		if sendPaquetWithFiability(0.95) == true {
-			if n > 0 {
-				//fmt.Println("Dans if")
-				if string(buff[:n]) == "END" {
-					fmt.Println("END")
-					break
-				}
-				nbPacket, buffbody := decapPacket(buff[:n])
+	buff := make([]byte, 1024)
+	var packetMap map[uint64]int
 
-				//str = str + string(buff[:n])
-				if isDuplicatePacket(packetMap, nbPacket) == false {
-					str = str + string(buffbody)
+	packetMap = make(map[uint64]int)
+	str := ""
+
+	conn.Write([]byte("Client - CONNEXION OK"))
+	fmt.Println("-------------------------")
+	fmt.Printf("Connected on : %s\n\n", name)
+
+	if newServerConnexion(conn) == 0 {
+		for {
+			fmt.Println("-------------------------")
+			n, err := conn.Read(buff)
+			if err != nil {
+				fmt.Println("ERROR")
+				break
+			}
+
+			if sendPaquetWithFiability(0.95) == true {
+				if n > 0 {
+					if string(buff[:n]) == "END" {
+						fmt.Println("END")
+						break
+					}
+					nbPacket, buffbody := decapPacket(buff[:n])
+
+					if isDuplicatePacket(packetMap, nbPacket) == false {
+						str = str + string(buffbody)
+					} else {
+						log.Printf("Packet nb : %v DUPLICATE\n", nbPacket)
+					}
+
+					conn.Write([]byte("PACKAGE RECEIVE"))
+					log.Printf("Send: %s\n", "PACKAGE RECEIVE")
+					log.Printf("Package nb : %v\n", nbPacket)
+					getByteSignature(buffbody)
+
+					fmt.Println("-------------------------")
+
 				} else {
-					log.Printf("Packet nb : %v DUPLICATE\n", nbPacket)
+					continue
 				}
-
-				conn.Write([]byte("PACKAGE RECEIVE"))
-				log.Printf("Send: %s\n", "PACKAGE RECEIVE")
-				log.Printf("Package nb : %v\n", nbPacket)
-				// log.Println("Packet complet")
-				// getByteSignature(buff[:n])
-				// log.Println("numero Packet")
-				// getByteSignature(buff[:8])
-				log.Println("corp du Packet")
-				//getByteSignature(buff[8:n])
-				getByteSignature(buffbody)
-
-				fmt.Println("-------------------------")
-
 			} else {
-				fmt.Println("Dans else")
+				log.Println("Fiability Error")
 				continue
 			}
-		} else {
-			log.Println("Fiability Error")
-			continue
 		}
-		//log.Printf("Receive: %s\n", buff[:n])
-		//log.Printf("Final packet : %s\n", str)
-		//buff = nil
-
 	}
-	//convertBytesToFile("packet.jpeg", []byte(str), 0644)
-	fmt.Println(packetMap)
-	convertBytesToFile("packet.jpg", []byte(str), 0644)
+	convertBytesToFile("packet.jpeg", []byte(str), 0644)
 	getFileByteSignature([]byte(str))
 }
